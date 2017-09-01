2a3feb51c10fc7661515b12ac18731b1d7ee1629
Mark Herhold <mark.herhold@gmail.com>
2017-09-01 00:24:31 -0400

    chrome headless

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,40 +1,18 @@
 // Karma configuration
 
+// puppeteer will download Chrome headless at install time
+var ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision;
+var Downloader = require('puppeteer/utils/ChromiumDownloader');
+var revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision);
+
+process.env.CHROME_BIN = revisionInfo.executablePath;
+
 module.exports = function (config) {
-
-  var customLaunchers = {
-    sl_chrome: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 10',
-      version: '60'
-    },
-    sl_firefox: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '54'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    },
-    sl_android: {
-      base: 'SauceLabs',
-      browserName: 'Browser',
-      platform: 'Android',
-      version: '4.4',
-      deviceName: 'Samsung Galaxy S3 Emulator',
-      deviceOrientation: 'portrait'
-    }
-  };
-
   config.set({
     basePath: './..',
-    frameworks: [ 'browserify', 'tap'],
+    frameworks: [ 'browserify', 'tap' ],
     preprocessors: {
-      './test/**/*.js': ['browserify']
+      './test/**/*.js': [ 'browserify' ]
     },
     files: [
       './test/**/*.js'
@@ -42,20 +20,17 @@ module.exports = function (config) {
     browserify: {
       debug: true,
       transform: [],
-      extensions: ['.js']
+      extensions: [ '.js' ]
     },
-    exclude: ['./test/karma.conf.js', './test/browser/**/*'],
-    reporters: ['progress'],
+    exclude: [ './test/karma.conf.js' ],
+    reporters: [ 'progress' ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_WARN,
     autoWatch: false,
-    customLaunchers: customLaunchers,
-    browsers: Object.keys(customLaunchers), // can also test with the browser locally by using ['Chrome']
+    browsers: [ 'ChromeHeadless' ],
     singleRun: true,
     concurrency: 2,
-    retryLimit: 2,
-    captureTimeout: 120 * 1000,
-    browserNoActivityTimeout: 120 * 1000
+    retryLimit: 2
   });
 };

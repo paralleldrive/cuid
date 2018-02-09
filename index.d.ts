c8965172023189581a11a5bc4c47c66706554ae7
GP <ganeshp@indix.com>
2018-02-09 16:42:30 +0530

    Better TypeSript definitions.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,20 @@
+/**
+ * Create a unique collision-resistant ID.
+*/
 declare const cuid: (() => string) & {
+  /**
+   * Create a shorter unique collision-resistant ID.
+   * Is 7 to 10 characters in length.
+   */
   slug: (() => string)
+  /**
+   * Check if string is a valid 'cuid'.
+   */
+  isCuid: (() => boolean)
+  /**
+   * Check if string is a valid 'cuid' slug.
+   */
+  isSlug: (() => boolean)
 };
 
 export = cuid;

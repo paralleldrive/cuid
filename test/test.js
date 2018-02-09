3c447b49160a4d0afae1090aab6f318b1cc9c492
GP <ganeshp@indix.com>
2018-02-09 16:42:54 +0530

    Include tests for isCuid() and isSlug() methods.

--- test/test.js
+++ test/test.js
@@ -47,6 +47,23 @@ test('cuid.slug()', function (t) {
   t.end();
 });
 
+test('cuid.isCuid()', function (t) {
+  var id = cuid();
+  t.ok(cuid.isCuid(id) === true, 'cuid.isCuid() should return true for a valid cuid.');
+  t.ok(cuid.isCuid(null) === false, 'cuid.isCuid() should return false for null.');
+  t.ok(cuid.isCuid(undefined) === false, 'cuid.isCuid() should return false for undefined.');
+  t.ok(cuid.isCuid('abcdefghijklmnopqrstuvwxy') === false, 'cuid.isCuid() should return false for a random string.');
+  t.end();
+});
+
+test('cuid.isSlug()', function (t) {
+  var slug = cuid.slug();
+  t.ok(cuid.isSlug(slug) === true, 'cuid.isSlug() should return true for a valid cuid slug.');
+  t.ok(cuid.isSlug(null) === false, 'cuid.isSlug() should return false for null.');
+  t.ok(cuid.isSlug(undefined) === false, 'cuid.isSlug() should return false for undefined.');
+  t.end();
+});
+
 // perform collision test only if we aren't in the browser
 test('cuid colissions', { skip: isInBrowser }, function (t) {
   t.ok(collisionTest(cuid),

1c2b59f1c6d9dcfa126c025c173755b38c958525
GP <exchequer598@gmail.com>
2018-02-10 22:41:18 +0530

    More comments.

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,22 @@
 /**
  * Create a unique collision-resistant ID.
-*/
+ * Read more about cuid here - https://github.com/ericelliott/cuid
+ */
 declare const cuid: (() => string) & {
   /**
-   * Create a shorter unique collision-resistant ID.
-   * Is 7 to 10 characters in length.
+   * Create a shorter unique collision-resistant ID. Is 7 to 10 characters in length.
    */
   slug: (() => string)
   /**
    * Check if string is a valid 'cuid'.
+   *
+   * @param cuid: string to check if it is a 'cuid'.
    */
   isCuid: ((string) => boolean)
   /**
    * Check if string is a valid 'cuid' slug.
+   *
+   * @param slug: string to check if it is a 'cuid' slug.
    */
   isSlug: ((string) => boolean)
 };

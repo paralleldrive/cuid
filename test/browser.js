1f7e719921adb8d8ea1350f7faab16d0549a4747
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-08-18 00:53:42 -0700

    some cleanup

--- test/browser.js
+++ test/browser.js
@@ -9,14 +9,13 @@ const cafeToTapeFormat = (describe, ...rest) => {
   const fn = rest[1] ? rest[1] : rest[0]
   const options = rest[1] ? Object.assign({}, defaultOptions, rest[0]) : defaultOptions
 
-  return test (describe, async t => {
-    const ok = async (bool, message) => {
+  test(describe, t => fn({
+    ok: async (bool, message) => {
       if (options.skip) return;
-      await t.expect(bool).eql(true, message)
-    }
-    const end = async () => {}
-    return fn({ ok, end })
-  });
+      return await t.expect(bool).eql(true, message)
+    },
+    end: async () => {}
+  }));
 };
 
 cuidTests.run(cafeToTapeFormat, { isInBrowser: true })
